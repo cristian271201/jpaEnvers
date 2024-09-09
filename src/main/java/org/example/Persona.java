2f9fc116470e6230c8f969ca240dd534481e5a85
Alberto Cortez <cortezalberto@gmail.com>
2024-09-08 23:08:45 -0300

    Update Persona.java

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -3,53 +3,32 @@ package org.example;
 
 
 
+import lombok.*;
 import org.hibernate.envers.Audited;
 
 import javax.persistence.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+@Builder
 @Entity
 @Table(name = "Persona")
+
 @Audited
 public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private int age;
+    private String nombre;
+    private int edad;
 
     // Constructor, getters y setters
 
 
-    public Persona(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 
-    public Persona() {
 
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
